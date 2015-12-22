a45a71cd4fc9d9262001e1c82334fbeb76383c63
Tony Holdstock-Brown <tonyhb@gmail.com>
2015-12-21 23:44:40 -0800

    Add redux-simple-router; basic route state in store

--- src/scripts/reducers/index.js
+++ src/scripts/reducers/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
-import { combineReducers } from 'redux';
 // Import all reducers and export a single combined reducer
+import { combineReducers } from 'redux';
 import example from './example';
+import { routeReducer } from 'redux-simple-router';
 
 export default combineReducers({
+  router: routeReducer,
+  // app-specific reducers
   example
 });
